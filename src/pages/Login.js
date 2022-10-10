6d5ba068eedfdba5f49a890be9c0db6f55397bf0
seok <qhtjr7583@naver.com>
2022-10-10 21:32:12 +0900

    테스트

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Login(props) {
     return (
         <div>
-            <h3 align = "center">나는 로그인이야</h3>
+            <h3 align = "center">로그인이야</h3>
         </div>
     );
 }
