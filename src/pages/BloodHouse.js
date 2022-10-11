523272c2077e716e1eb3b401f7ab11eee2e29b9e
seok <qhtjr7583@naver.com>
2022-10-11 22:40:56 +0900

    hi

--- src/pages/BloodHouse.js
+++ src/pages/BloodHouse.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import useGeolocation from "react-hook-geolocation";
 
-function BloodHouse(props) {
-    return (
-        <div>
-            <h3 align = "center">나는 헌혈의 집이야</h3>
-        </div>
-    );
+function BloodHouse(){
+  return (
+    <Map 
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{width: "70%", marginLeft:"250px",marginTop:"100px",height: "500px" }}
+    >
+      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+        <div style={{color:"#000"}}>Hello World!</div>
+      </MapMarker>
+    </Map>
+  )
 }
 
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(position) {
+    console.log('위도 : ' + position.coords.latitude); 
+    console.log('경도: ' + position.coords.longitude);
+  };
+  
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+
+  
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
 export default BloodHouse;
